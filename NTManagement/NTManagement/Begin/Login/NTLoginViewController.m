e39375f4e67b4e376004abccb554b5afbd17304f
郑祯 <2745674085@qq.com>
2016-06-25 02:49:44 +0800

    登录界面的UITableView创建中...

--- NTManagement/NTManagement/Begin/Login/NTLoginViewController.m
+++ NTManagement/NTManagement/Begin/Login/NTLoginViewController.m
@@ -8,7 +8,9 @@
 
 #import "NTLoginViewController.h"
 
-@interface NTLoginViewController ()
+@interface NTLoginViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
 
 @end
 
@@ -16,12 +18,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    self.myTableView.dataSource = self;
+    self.myTableView.delegate = self;
+//    self.myTableView.scrollEnabled = NO;
 }
+
+#pragma mark - methods
 - (IBAction)back:(id)sender {
     [self dismissViewControllerAnimated:YES completion:^{
         
     }];
 }
+
+#pragma mark - dataSource
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    cell.textLabel.text = @"nineteen";
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+
+#pragma mark - delegate
+// 下面两个方法目的是为了取消header和footer
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 0.02;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 0.02;
+}
+
+
 
 @end
