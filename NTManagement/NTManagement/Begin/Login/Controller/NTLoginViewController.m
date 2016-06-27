0f752a3c38d167560d188781139d4414f873c205
郑祯 <2745674085@qq.com>
2016-06-27 15:28:09 +0800

    需求变化中（取消注册界面，只有登陆界面，并通过短信验证码验证登陆）

--- NTManagement/NTManagement/Begin/Login/Controller/NTLoginViewController.m
+++ NTManagement/NTManagement/Begin/Login/Controller/NTLoginViewController.m
@@ -14,7 +14,6 @@
 @property (weak, nonatomic) IBOutlet UITableView *myTableView;
 @property (weak, nonatomic) IBOutlet UIButton *forgetPasswordButton;
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
-@property (weak, nonatomic) IBOutlet UIButton *dismissButton;
 
 @end
 
@@ -33,7 +32,6 @@
     self.view.backgroundColor = [UIColor colorWithWhite:0.96 alpha:1.0];
     
     [self.forgetPasswordButton addTarget:self action:@selector(forgetPasswordAction) forControlEvents:UIControlEventTouchUpInside];
-    [self.dismissButton addTarget:self action:@selector(dismissAction) forControlEvents:UIControlEventTouchUpInside];
     
     
 }
