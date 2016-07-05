54e043bb5b1ccf98bec69783967a7d1ef8f0f531
郑祯 <2745674085@qq.com>
2016-07-05 20:49:10 +0800

    进一步修改效果中..

--- NTManagement/NTManagement/Primary/Information/Controller/NTInformationViewController.m
+++ NTManagement/NTManagement/Primary/Information/Controller/NTInformationViewController.m
@@ -9,6 +9,7 @@
 #import "NTInformationViewController.h"
 
 @interface NTInformationViewController () <UITableViewDelegate, UITableViewDataSource>
+
 @property (weak, nonatomic) IBOutlet UITableView *myTableView;
 @property (nonatomic, strong) UIImageView *myImageView;
 
@@ -16,29 +17,27 @@
 
 @implementation NTInformationViewController
 
+const static int imageViewHeight = 100;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.myTableView.delegate = self;
     self.myTableView.dataSource = self;
-    self.myTableView.contentInset = UIEdgeInsetsMake(320, 0, 0, 0);// 让cell向下移动100
+    self.myTableView.contentInset = UIEdgeInsetsMake(imageViewHeight, 0, 0, 0);// 让cell向下移动100
     [self.myTableView addSubview:self.myImageView];
 }
+
 
 #pragma mark - 懒加载
 - (UIImageView *)myImageView {
     if (_myImageView == nil) {
-        _myImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, -345, NTScreenWidth, 345)];
-        _myImageView.image = [UIImage imageNamed:@"xiang"];
-        _myImageView.contentMode = UIViewContentModeScaleAspectFill;
-
-//        _myImageView.alpha = 0.5;
+        _myImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, -imageViewHeight, NTScreenWidth, imageViewHeight)];
+        _myImageView.image = [UIImage imageNamed:@"阿狸"];
+        _myImageView.contentMode = UIViewContentModeScaleAspectFill;// 使用这个模式，放大的时候就会从中间进行放大
         [self.myTableView addSubview:_myImageView];
     }
     return _myImageView;
 }
-
-
-
 
 #pragma mark - dataSource
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -78,21 +77,17 @@
     }
 }
 
+// 制作拉伸效果主要的方法
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     CGFloat offsetY = scrollView.contentOffset.y;
-    NSLog(@"%f",offsetY);
-    CGFloat offsetH = 320 + offsetY;// offsetH是相对的偏移量(偏移量应该是scrollView.contentOffset.y 但是rootTableView.contentInset = UIEdgeInsetsMake(imgHeight, 0, 0, 0)  tableView提前设置便宜了一段距离  所以现在相对的偏移量应该是offsetH)
-    if (offsetH < 0) {//下拉偏移为负数
-        if (offsetH < -25) {
-            CGRect frame = self.myImageView.frame;
-            frame.size.height = 320 - offsetH;//下拉后图片的高度应变大
-            frame.origin.y = -320 + offsetH;// 下边界是一定的  高度变大了  起始的Y应该上移
-            self.myImageView.frame = frame;
-        } else {
+    CGFloat offsetH = imageViewHeight + offsetY;
+    if (offsetH < 0) { //向下拉动的时候调用
+        if (offsetH > -25) {// 当0到-25这个时段，不进行拉伸效果
             
+        } else {
+            self.myImageView.height = imageViewHeight - offsetH;// 将高度调高
+            self.myImageView.y = -imageViewHeight + offsetH;// 下边界是一定的，高度变大了，起始的Y应该上移
         }
-        
-        
     }
 }
 @end
