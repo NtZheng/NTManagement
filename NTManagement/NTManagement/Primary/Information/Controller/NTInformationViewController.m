db38a5cc82ba094969cd614b193c82a08ccf2f58
NtZheng <2745674085@qq.com>
2016-07-30 14:35:22 +0800

    做了个人界面导航栏渐变透明度的效果（第一次做的时候失败了，坑啊，没有看到storyboard中图片的位置，不知道为什么顶部就是有一块空白，晕晕晕）

--- NTManagement/NTManagement/Primary/Information/Controller/NTInformationViewController.m
+++ NTManagement/NTManagement/Primary/Information/Controller/NTInformationViewController.m
@@ -17,13 +17,15 @@
 
 @implementation NTInformationViewController
 
-const static int imageViewHeight = 100;
+const static int imageViewHeight = 150;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.myTableView.delegate = self;
     self.myTableView.dataSource = self;
+    self.automaticallyAdjustsScrollViewInsets = NO;
     self.myTableView.contentInset = UIEdgeInsetsMake(imageViewHeight, 0, 0, 0);// 让UITableView的内容向下偏移imageViewHeight这么多距离，空出来用于背景显示
+    self.navigationController.navigationBarHidden = YES;
 }
 
 #pragma mark - dataSource
@@ -69,6 +71,10 @@ const static int imageViewHeight = 100;
     if (offsetH < imageViewHeight) {// 向下拉动的时候调用
         self.myImageView.height = imageViewHeight - offsetH;// 改变UIImageView的高度（重点：由于我们设置的UIImageView的模式为Aspect Fill，所以系统会这样做->当还没有完全显示UIImageView的时候进行显示，这种显示效果就是UITableView的内容的变化速度看起来比UIImageView的变化速度要快，这是系统的效果，不需要我们实现，接着，当显示完全的时候，那么就按照Aspect Fill这种效果去拉伸图片）
     }
+    if (offsetH != 0) {
+        self.navigationController.navigationBarHidden = NO;
+    }
+    self.navigationController.navigationBar.alpha = offsetH/(imageViewHeight - 64);;
 }
 
 @end
